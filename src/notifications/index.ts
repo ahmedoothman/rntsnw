eb6e697c92a5af1018a0be54823b8be920a496ef
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2025-06-25 19:46:13 +0300

    fixed issue

--- src/notifications/index.ts
+++ src/notifications/index.ts
@@ -2,7 +2,6 @@
 export {default as NotificationContainer} from '../components/NotificationContainer';
 export {useNotification} from '../hooks/useNotification';
 export {NotificationService} from '../utils/NotificationService';
-export {default as NotificationExample} from '../components/NotificationExample';
 
 // Redux exports
 export {
