1472a896d2425236bd2fb84a4597436ea57a8adf
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2025-07-08 21:07:33 +0300

    added react query

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {store} from './src/redux/store';
 import AppWrapper from './src/components/AppWrapper';
 
@@ -10,11 +11,23 @@ if (__DEV__) {
   require('./src/config/ReactotronConfig');
 }
 
+// Create a client for React Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
+
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <AppWrapper />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AppWrapper />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
